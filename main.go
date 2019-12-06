fedfae020dedd72b045ca04942078de130a7c8c6
Víctor Vázquez <victorvazrod@gmail.com>
2019-12-06 16:57:18 +0100

    Move init function to main

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"log"
 
-	"github.com/varrrro/gonn/internal/solution"
+	"github.com/varrrro/gonn/internal/layer"
+	"github.com/varrrro/gonn/internal/model"
 	"github.com/varrrro/gonn/internal/util"
+	"gonum.org/v1/gonum/mat"
 )
 
 const (
@@ -40,5 +42,29 @@ func main() {
 	trainImgs := util.NormalizeImages(&rawTrainImgs)
 	testImgs := util.NormalizeImages(&rawTestImgs)
 
-	solution.InitMultilayer(&trainImgs, &testImgs, &trainLbls, &testLbls)
+	initNetwork(&trainImgs, &testImgs, &trainLbls, &testLbls)
+}
+
+func initNetwork(trainImgs, testImgs *[]mat.Vector, trainLabels, testLabels *[]int) {
+	// Initialize weight and bias arrays
+	hiddenWeights := util.InitializeRandom(784 * 256)
+	hiddenBiases := util.InitializeRandom(256)
+	outputWeights := util.InitializeRandom(256 * 10)
+	outputBiases := util.InitializeRandom(10)
+
+	// Create the layers
+	hiddenLayer := layer.CreateSigmoidalLayer(784, 256, hiddenWeights, hiddenBiases)
+	outputLayer := layer.CreateSoftmaxLayer(256, 10, outputWeights, outputBiases)
+
+	// Create the nerwork parameters
+	params := model.CreateParameters(0.1)
+
+	// Create the network
+	nn := model.Network{Layers: []model.Layer{hiddenLayer, outputLayer}, Params: params}
+
+	// Train the network with the training set for a number of epochs
+	nn.Train(*trainImgs, *trainLabels, 10)
+
+	// Test the network with the test set
+	nn.Test(*testImgs, *testLabels)
 }
